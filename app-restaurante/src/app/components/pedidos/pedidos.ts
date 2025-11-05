d3351cb0031b210a40d8f9fb351fdde30da7d7b6
Lucas Vinícius Teixeira de Góis <lu.viniteixeira78@gmail.com>
2025-11-05 11:15:19 +0000

    fix:apagando código nao utilizadp

--- app-restaurante/src/app/components/pedidos/pedidos.ts
+++ app-restaurante/src/app/components/pedidos/pedidos.ts
@@ -45,31 +45,6 @@ export class Pedidos implements OnInit {
     ]
   }
 
-  postPutPedido() {
-
-    this.visualizando = false
-
-    if (this.novoPedido.id !== 0) {
-      const index = this.pedidos.findIndex((p:any) => p.id === this.novoPedido.id);
-      if (index !== -1) {
-        this.pedidos[index] = { ...this.novoPedido };
-      }
-    } 
-    else {
-      const novoId = this.pedidos.length > 0 ? this.pedidos[this.pedidos.length - 1].id + 1 : 1;
-      this.pedidos.push({ ...this.novoPedido, id: novoId });
-    }
-
-    this.novoPedido = {
-      id: 0,
-      cliente: '',
-      produto: '',
-      quantidade: 1,
-      status: 'Preparando'
-    };
-    this.visible = false; 
-  }
-
   abrirEditarPedido(pedido:any){
     this.novoPedido = {...pedido}
     this.visible = true
